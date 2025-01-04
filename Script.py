c0cb020790baeb3b3d4718255c4953ff06189801
YashYasag <yevettenegush.a.mw612.1@gmail.com>
2025-01-04 12:40:40 +0530

    Update Script.py

--- Script.py
+++ Script.py
@@ -17,7 +17,7 @@ sɪᴢᴇ ⚙️: {file_size}
 
 Jᴏɪɴ [ᴜᴘᴅᴀᴛᴇ ᴄʜᴀɴɴᴇʟ](https://t.me/vj_botz)</b>""" 
 
-    CAPTIONOP = """<b>📂 ғɪʟᴇɴᴀᴍᴇ : {view_caption}
+    CAPTIONOP = """<b>📂 ғɪʟᴇɴᴀᴍᴇ : {file_caption}
 
 sɪᴢᴇ ⚙️: {file_size}
 
