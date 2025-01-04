ddc9bcff01cf175ab037a4e41864185f841bab2b
YashYasag <yevettenegush.a.mw612.1@gmail.com>
2025-01-04 12:44:18 +0530

    Update commands.py

--- plugins/commands.py
+++ plugins/commands.py
@@ -199,7 +199,7 @@ async def start(client, message):
                     chat_id=message.from_user.id,
                     file_id=msg.get("file_id"),
                     caption=f_caption,
-                    protect_content=msg.get('protect', False),
+                    protect_content=OP,
                     reply_markup=InlineKeyboardMarkup(button)
                 )
                 filesarr.append(msg)
